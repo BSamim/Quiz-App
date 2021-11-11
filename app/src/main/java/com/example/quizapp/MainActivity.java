ffbd2c205f8c9758b382154ac6826f94d1b44abf
Samim Zubair <bsamimzubair@gmail.com>
2021-11-11 20:23:28 +0500

    Repo Intent Url Added

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button repo;
+    Button app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repo.findViewById(R.id.button_repo);
+        app.findViewById(R.id.button_app);
+        repo.setOnClickListener(this::onClick);
+    }
+
+    private void onClick(View v) {
+        String url = "https://github.com/BSamim/Quiz-App";
+        Intent openRepo = new Intent(Intent.ACTION_VIEW);
+        openRepo.setData(Uri.parse(url));
+        startActivity(openRepo);
     }
 }
