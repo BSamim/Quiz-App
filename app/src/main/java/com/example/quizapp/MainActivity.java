d922971a2a9aff4dc985a92787d289bffaaf843d
Samim Zubair <bsamimzubair@gmail.com>
2021-11-11 22:14:10 +0500

    Main and welcome page created

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,25 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button repo;
-    Button app;
+    Button github_repo;
+    Button quiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        repo.findViewById(R.id.button_repo);
-        app.findViewById(R.id.button_app);
-        repo.setOnClickListener(this::onClick);
-    }
 
-    private void onClick(View v) {
-        String url = "https://github.com/BSamim/Quiz-App";
-        Intent openRepo = new Intent(Intent.ACTION_VIEW);
-        openRepo.setData(Uri.parse(url));
-        startActivity(openRepo);
+        github_repo=findViewById(R.id.button_repo);
+        quiz=findViewById(R.id.button_app);
+        github_repo.setOnClickListener(v -> {
+            String url = "https://github.com/BSamim/Quiz-App";
+            Intent openRepo = new Intent(Intent.ACTION_VIEW);
+            openRepo.setData(Uri.parse(url));
+            startActivity(openRepo);
+        });
+        quiz.setOnClickListener(v ->{
+            Intent X = new Intent(getApplicationContext(), welcome.class);
+            startActivity(X);
+        });
     }
 }
