8b5c9663890895c1e520403efc18a620a185d755
Samim Zubair <bsamimzubair@gmail.com>
2021-11-11 22:59:40 +0500

    Welcome page created and Revise Button functionality Added

--- app/src/main/java/com/example/quizapp/welcome.java
+++ app/src/main/java/com/example/quizapp/welcome.java
@@ -2,13 +2,26 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class welcome extends AppCompatActivity {
+    Button proceed_btn;
+    Button rev_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        rev_btn=findViewById(R.id.revise_btn);
+        rev_btn.setOnClickListener(v -> {
+            String url = "https://useenglishwords.com/results/makharij-al-huroof/";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
     }
 }
